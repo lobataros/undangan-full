5d9d61c4531e4138cee4a69f2f84a9d2ac09bff8
Dimas <dimashpt@gmail.com>
2023-04-13 20:16:04 +0700

    update viewport

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=1024">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $guest?->nama ?? 'Undangan' }} - Pernikahan Dimas & Ira</title>
     <meta name="title" content="Undangan Pernikahan Dimas & Ira">
     <meta name="description" content="Website Undangan Elektronik Pernikahan Dimas & Ira">
